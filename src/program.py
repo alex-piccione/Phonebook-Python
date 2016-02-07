67b9d4cec1d3086927a9a1d4e7a6c945b99ea04a
Alessandro Piccione <alex.75@tin.it>
2016-02-07 20:02:22 +0100

    Use private function for read and write from UI. Add SetUp on test.

--- src/program.py
+++ src/program.py
@@ -1,14 +1,29 @@
 
 class Program:
 
+    def __init__(self):
+
+        self.menu = """Select:
+   1. Search number
+   9. Quit
+"""
+
     def run(self):
 
         self.showMenu()
 
     def showMenu(self):
 
-        menu = """"Select:
-               1. Search number
-               9. Quit
-               """
-        print(menu)
+        self._write(self.menu)
+
+        selection = self._read()
+
+    def getList(self):
+
+        return []
+
+    def _read(self, text=None):
+        return input(text)
+
+    def _write(self, text):
+        print()
--- test/ProgramUnitTest.py
+++ test/ProgramUnitTest.py
@@ -3,27 +3,37 @@ from unittest.mock import MagicMock
 import sys
 from io import StringIO
 
-import src.program
+from src.program import Program
 
 
 class ProgramTest (unittest.TestCase):
 
-    def test_run_exist(self):
+    def setUp(self):
+        self.program = Program()
+        self.program._read = MagicMock(return_value="")
+        self.program._write = MagicMock()
 
-        program = src.program.Program()
-        program.run()
+    def test_menu__should__exists(self):
+        assert hasattr(self.program, "menu")
 
-    def test_run_show_menu(self):
+    def test_run__should__exist(self):
+        hasattr(self.program, "run")
+        self.program.run()
 
-        # output = sys.stdout.getline().strip()
-        outSpy = StringIO()
-        sys.stdout = outSpy
+    def test_run__should__show_menu_and_wait_for_input(self):
+        expectedText = self.program.menu
 
-        program = src.program.Program()
-        program.run()
-        output = outSpy.getvalue().strip()
+        self.program.run()
 
-        assert output.__contains__("Select")
+        self.program._write.assert_called_once_with(expectedText)
 
+        self.program._read.assert_called_once_with()
 
+    def test_get_list__should__exists(self):
+        assert hasattr(self.program, "getList")
 
+    def test_get_list__should__return_a_list(self):
+        phonebook = self.program.getList()
+        self.assertIsNotNone(phonebook)
+        self.assertIsInstance(phonebook, list)
+        #assert hasattr(phonebook, "__iter__"); # is iterable
